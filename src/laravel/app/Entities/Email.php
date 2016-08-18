9c5c433c6ecca146e259cef12da7a8ee9ec8ef0a
skvoz <skvoz.ne@gmail.com>
2016-08-18 20:14:30 +0300

    add entities for user and email

---
+++ src/laravel/app/Entities/Email.php
@@ -0,0 +1,116 @@
+<?php
+namespace App\Entities;
+
+use Doctrine\ORM\Mapping AS ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="email")
+ */
+class Email
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $to;
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $subject;
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $body;
+    /**
+     * @ORM\OneToMany(targetEntity="Users", mappedBy="users", cascade={"persist"})
+     * @var ArrayCollection|Users[]
+     */
+    protected $user;
+
+
+    public function __construct()
+    {
+        $this->user = new ArrayCollection;
+    }
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTo()
+    {
+        return $this->to;
+    }
+
+    /**
+     * @param mixed $to
+     */
+    public function setTo($to)
+    {
+        $this->to = $to;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+    /**
+     * @param mixed $subject
+     */
+    public function setSubject($subject)
+    {
+        $this->subject = $subject;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * @param mixed $body
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+    }
+
+    /**
+     * @return ArrayCollection|Users[]
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+
+}
